4e61d51ea3fb4217dd77cc5bcb406b7a03a220f9
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2019-08-12 11:13:26 +0200

    add console

---
+++ startrek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace startrek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
